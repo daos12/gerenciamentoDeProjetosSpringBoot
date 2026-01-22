e0e1156ef1720d62405a7dc1f654d03536b7dd24
Diego Antonio <daos_ptc@hotmail.com>
2026-01-22 16:56:49 -0300

    first commit

---
+++ src/main/java/com/diego/GerenciamentoTarefas/GerenciamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.diego.GerenciamentoTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasApplication.class, args);
+	}
+
+}
